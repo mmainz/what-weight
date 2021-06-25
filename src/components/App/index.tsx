42ae55b3ae38a5217dcb6bc0693723cf7ada0613
Mario Mainz <mario.mainz@invision.de>
2021-06-25 21:25:39 +0200

    Add heading

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,6 +3,7 @@ import {
   Container,
   Grid,
   GridItem,
+  Heading,
   Text,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
@@ -16,23 +17,9 @@ export const App: React.FC = () => {
   return (
     <ChakraProvider>
       <Container>
-        <Grid templateColumns="1fr 2fr 2fr" templateRows="1fr 1fr" gap={2}>
-          <GridItem
-            colStart={1}
-            rowStart={1}
-            display="flex"
-            justifyContent="stretch"
-            alignItems="center"
-          >
-            <Text fontSize="xl" whiteSpace="nowrap" textAlign="center">
-              Weight lifted:
-            </Text>
-          </GridItem>
-          <GridItem colStart={2} rowStart={1}>
-            <WeightInput
-              weight={liftedWeight}
-              onWeightChanged={setLiftedWeight}
-            />
+        <Grid templateColumns="1fr 2fr" templateRows="1fr 1fr 1fr" gap={2}>
+          <GridItem colStart={1} colEnd={3} rowStart={1}>
+            <Heading>what weight!?</Heading>
           </GridItem>
           <GridItem
             colStart={1}
@@ -41,11 +28,28 @@ export const App: React.FC = () => {
             justifyContent="stretch"
             alignItems="center"
           >
+            <Text fontSize="xl" whiteSpace="nowrap" textAlign="center">
+              Weight lifted:
+            </Text>
+          </GridItem>
+          <GridItem colStart={2} rowStart={2}>
+            <WeightInput
+              weight={liftedWeight}
+              onWeightChanged={setLiftedWeight}
+            />
+          </GridItem>
+          <GridItem
+            colStart={1}
+            rowStart={3}
+            display="flex"
+            justifyContent="stretch"
+            alignItems="center"
+          >
             <Text fontSize="xl" whiteSpace="nowrap">
               Reps performed:
             </Text>
           </GridItem>
-          <GridItem colStart={2} rowStart={2}>
+          <GridItem colStart={2} rowStart={3}>
             <RepsInput reps={repsPerformed} onRepsChanged={setRepsPerformed} />
           </GridItem>
         </Grid>
