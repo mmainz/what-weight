26cf461fff6599ce75998a83be1a4cfc2c25dc35
Mario Mainz <mario.mainz@invision.de>
2021-06-26 21:22:50 +0200

    Add WebPack build for production

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,26 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    /* ... */
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+  mount: {},
+  plugins: [['@snowpack/plugin-webpack', {}]],
+  routes: [],
+  optimize: {},
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
 };
