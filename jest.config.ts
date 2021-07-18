ac75f351fe2547eec4b271fee9d3084fce564dbb
Mario Mainz <mario.mainz@invision.de>
2021-07-18 11:23:08 +0200

    Add file mocks for jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,4 +6,8 @@ export default {
     '^.+\\.tsx?$': 'ts-jest',
   },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+  },
 };
